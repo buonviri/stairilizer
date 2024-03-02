5349a47a8136c571b25fdf1c11d29f1f7c9f4101
buonviri <andy@buonviri.com>
2024-03-02 16:50:33 -0500

    added some sort features

--- stairilizer.py
+++ stairilizer.py
@@ -1,16 +1,25 @@
 # this script cleans up step files in the working dir with the following actions:
+#   rename objects and file info
+#   perform brute force replacement of values that contain floating point errors
+#   perform range replacement of same (planned)
+#   remove unused lines (planned)
+#   condense duplicate lines such as points and vectors (planned)
+#   sort lines (started)
 
-# rename objects and file info
-# perform brute force replacement of values that contain floating point errors
-# perform range replacement of same (planned)
-# remove unused lines (planned)
-# condense duplicate lines such as points and vectors (planned)
-# sort lines (planned)
-#   look for ADVANCED_BREP_SHAPE_REPRESENTATION, may be multi-line
-
-# output = (strl).step
 
 import os
+
+
+def SplitSTEP(oldstring):
+    char = ';'  # set this to whatever character indicates the end of a line
+    newstring = oldstring.replace('\n', ' ')  # replace all newlines with space
+    newstring = newstring.replace('\r', ' ')  # replace all carriage returns with space
+    newlist = newstring.split(char)  # split into list based on char
+    strippedlist = []
+    for s in newlist:
+        strippedlist.append(s.strip())
+    return ';\n'.join(strippedlist)  # return list joined back into string
+# End SplitSTEP
 
 
 def FixFloatingPoint(oldstring):
@@ -20,6 +29,7 @@ def FixFloatingPoint(oldstring):
         """'Managed by Terraform'""":        """''""",
         """'SolidWorks 2017'""":             """''""",
         """'SwSTEP 2.0'""":                  """''""",
+        '95836A524_NO THREADS_Black-Oxide 18-8 Stainless Steel Pan Head Phillips Screws': 'Screw',
     }
     numberpairs = [
         ['1.000000000000000000',    '1.0'    ],
@@ -37,6 +47,30 @@ def FixFloatingPoint(oldstring):
         newstring = newstring.replace(k, fix[k])
     return newstring
 # End FixFloatingPoint
+
+
+def SortSTEP(oldstring):
+    newlist = oldstring.split('\n')  # split into list
+    header = []
+    absr = []  # ADVANCED_BREP_SHAPE_REPRESENTATION
+    mslb = []  # MANIFOLD_SOLID_BREP
+    clsh = []  # CLOSED_SHELL
+    lines = []
+    foundpound = False
+    for line in newlist:
+        if 'ADVANCED_BREP_SHAPE_REPRESENTATION' in line:
+            absr.append(line)
+        elif 'MANIFOLD_SOLID_BREP' in line:
+            mslb.append(line)
+        elif 'CLOSED_SHELL' in line:
+            clsh.append(line)
+        elif line.startswith('#') or foundpound == True:
+            lines.append(line)
+            foundpound = True
+        else:
+            header.append(line)
+    return '\n'.join(header + absr + mslb + clsh + lines)  # return list joined back into string
+# End SortSTEP
 
 
 def AnalyzeFiles(exts, suffix, funcs):
@@ -66,9 +100,9 @@ def AnalyzeFiles(exts, suffix, funcs):
 
 # --- Start of script ---
 
-AnalyzeFiles(['.STEP', '.STP', '.step', '.stp'], 'strl', [FixFloatingPoint,])
+AnalyzeFiles(['.STEP', '.STP', '.step', '.stp'], 'strl', [SplitSTEP,FixFloatingPoint,SortSTEP])
 
-print()
+# print()
 # os.system('PAUSE')
 
 #EOF
